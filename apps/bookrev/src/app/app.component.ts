3156939c11dae42ce3a798919a338894ab87affb
Wiktor Dębski <dewiktor@gmail.com>
2022-02-06 15:29:29 +0100

    Config husky

--- apps/bookrev/src/app/app.component.ts
+++ apps/bookrev/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Message } from '@fs-workspace/api-interfaces';
-
 @Component({
     selector: 'fs-workspace-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-    hello$ = this.http.get<Message>('/api/hello')
-    constructor(private http: HttpClient) {};
+    hello$ = this.http.get<Message>('/api/hello');
+    constructor(private http: HttpClient) {
+        console.log('');
+    };
 }
